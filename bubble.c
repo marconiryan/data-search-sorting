7f4443addde471c073f2aa7ba342a1d4f59ec686
Ryan <86175804+marconiryan@users.noreply.github.com>
2022-05-12 15:13:14 -0300

    bubble

---
+++ bubble.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void swap(int *x, int *y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+
+
+}
+
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  int vetor[n];
+  for(int i = 0; i < n; i++){scanf("%d",&vetor[i]);}
+  for(int i = n-1; i > 0; i--){
+    for(int j = 0; j < i; j++){
+        if(vetor[j] > vetor[j+1]){
+          swap(&vetor[j],&vetor[j+1]);
+
+        }
+
+
+    }
+
+  }
+  for(int i = 0; i < n; i++){
+    printf("%d\n",vetor[i]);
+  }
+
+
+
+  return 0;
+}
+
+
